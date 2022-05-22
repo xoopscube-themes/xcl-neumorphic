0c590d76cf1e69b704316bef80e3e8704305a008
gigamaster <1905497+gigamaster@users.noreply.github.com>
2022-05-22 19:07:14 +0200

    🎨 [ WIP ] Update Theme Neumorphic for XCL

---
+++ html/themes/xcl-neumorphic/manifesto.ini.php
@@ -0,0 +1,22 @@
+<?php
+/**
+
+[Manifesto]
+Name="XCL Neumorphic"
+Depends=Legacy_RenderSystem,legacy
+ThemeOptions="index.php"
+Url="https://github.com/xoopscube-themes/xcl-neumorphic"
+Version="2.3.1"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author="Nuno Luciano aka Gigamaster"
+ScreenShot="screenshot.png"
+Description="Neumorphism design built with xLayout Flexbox, jQuery UI and CSS Custom properties."
+W3C=NG
+
+License="https://creativecommons.org/licenses/by/4.0/"
+
+*/
+
